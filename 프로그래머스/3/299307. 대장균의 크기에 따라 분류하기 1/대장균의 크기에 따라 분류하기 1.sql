8389deef93e7309b3146adb3c5637ffbc4595939
jh <34118304+LeeJeongHun1@users.noreply.github.com>
2024-08-11 16:34:54 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 1, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+
+select id,
+case
+    when SIZE_OF_COLONY <= 100
+    then 'LOW'
+    when SIZE_OF_COLONY > 100 && SIZE_OF_COLONY <= 1000
+    then 'MEDIUM'
+    when SIZE_OF_COLONY > 1000
+    then 'HIGH'
+end as SIZE
+from ECOLI_DATa
